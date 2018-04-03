34b1b11ed13a3ed34519686bb5a49a692de7f381
Ranjan Kalita <ranjankalita@bitbucket.org>
2018-04-03 15:09:01 +0530

    hib first commit- only pom

---
+++ HibernateMappingProj/src/main/java/prac/rk/hibernate/HibernateMappingProj/App.java
@@ -0,0 +1,13 @@
+package prac.rk.hibernate.HibernateMappingProj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
